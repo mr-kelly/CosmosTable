afb8efe9c3ad18775f64e0192ae8d3932ad4b521
mrkelly <chepy.v@gmail.com>
2015-03-29 23:22:43 +0800

    optimize project

--- CosmosConfigurator/CosmosConfiguratorCompiler/Compiler.cs
+++ CosmosConfigurator/CosmosConfiguratorCompiler/Compiler.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.IO;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using Excel;
